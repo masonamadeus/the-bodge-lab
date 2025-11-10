262c5fc07a5f4b104266f25a122cb27545d38aff
Mason Amadeus <71096260+masonamadeus@users.noreply.github.com>
2025-11-09 22:07:08 -0500

    maybe roll back

--- _data/meta.js
+++ _data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: "https://bodgelab.com/", 
   
   // Path to default OG image
-  defaultImage: "/config/default-og.webp",
+  defaultImage: "/config/sitebg2.webp",
   
   // Default description for pages without content
   defaultDescription: "The Bodge Lab: a curious folder on the internet."
